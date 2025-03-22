8c0ca92b5219656b9f6e2f6d852730c23b51e545
Seresun <slavnyunstoppable22@gmail.com>
2025-03-22 19:43:23 +0100

    fixl

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,6 +3,7 @@
 import createHttpError from 'http-errors';
 
 import { SessionsCollection } from '../db/models/session.js';
+
 import { UsersCollection } from '../db/models/user.js';
 
 export const authenticate = async (req, res, next) => {
